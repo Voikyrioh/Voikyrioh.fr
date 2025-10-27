ada62babb73a21cfeb0001bdba32d89bfb3a3404
Voikyrioh <voikyrioh@gmail.com>
2025-10-27 10:59:13 +0100

    Init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="flex justify-center items-center h-screen">
+  <h2 class="text-2xl">Content will be here</h2>
+</div>
+</template>
+
+<style scoped>
+
+</style>
